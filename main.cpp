f0b20cc0d3309d6c93819022464ce807440c8ce7
Milos <milosvuksanovic53@gmail.com>
2020-06-13 22:59:36 +1000

    First commit and comment of objective.

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+//the objective of this project is to recreate the Python Storage Tracker created very recently.
+//we need to translate it to C++
+int main()
+{   
+    std::cout << "Test comment!";
+
+}
