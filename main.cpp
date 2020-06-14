047d000d1d15d134a3fa02f853433e2b0a362ea8
Milos <milosvuksanovic53@gmail.com>
2020-06-14 23:31:42 +1000

    get_username implemented

--- main.cpp
+++ main.cpp
@@ -1,19 +1,27 @@
 #include<iostream>
 #include<Windows.h>
 #include<stdio.h>
+#include<tchar.h>
 //the objective of this project is to recreate the Python Storage Tracker created very recently.
 //we need to translate it to C++
 
+#define UNLEN 256
+
 void get_username()
 {
+    TCHAR user[UNLEN+1];
+    DWORD user_len = UNLEN+1;
+
+    GetUserName((TCHAR*)user, &user_len);
+    std::cout<< user << "\n";
 
 }
 
 void last_accesed()
 {
-    SYSTEMTIME lt;
+    SYSTEMTIME lt; //Get local time
 
-    GetSystemTime(&lt);
+    GetSystemTime( &lt );
     //show last accessed time Hr:Min:sec
     std::cout << "Last Accessed: " << lt.wHour << ":" << lt.wMinute << ":" << lt.wSecond <<"\n";
 }
@@ -28,12 +36,12 @@ void get_version()
     dwVersion = GetVersion();
     
     //get windows version
-    dwMajorVersion = (DWORD)(LOBYTE(LOWORD(dwVersion)));
-    dwMinorVersion = (DWORD)(HIBYTE(LOWORD(dwVersion)));
+    dwMajorVersion = ( DWORD )( LOBYTE ( LOWORD ( dwVersion )));
+    dwMinorVersion = ( DWORD )( HIBYTE ( LOWORD ( dwVersion )));
 
     //get build number
-    if (dwVersion < 0x80000000)
-        dwBuild = (DWORD)(HIWORD(dwVersion));
+    if ( dwVersion < 0x80000000 )
+        dwBuild = ( DWORD )( HIWORD ( dwVersion ));
 
     //printf("Version is %d.%d %d\n",dwMajorVersion, dwMinorVersion, dwBuild);    
      std::cout << "Version: " << dwMajorVersion << "." << dwMinorVersion << " " << dwBuild ;
@@ -42,6 +50,7 @@ void get_version()
 
 int main()
 {   
+    get_username();
     last_accesed();
     get_version();
 
